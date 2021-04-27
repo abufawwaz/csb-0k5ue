77f3d0c0ea02eb80ae73cc84e41f77ec689d8c9a
Code Sandbox <hello@codesandbox.io>
2021-04-27 03:39:00 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.1c317abc3464b7ea2d8822a5dc26b04a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad769e3edadfde11b5b00d55f7fd127e",
+    "url": "https://abufawwaz.github.io/csb-0k5ue/index.html"
+  },
+  {
+    "revision": "95b3eec258064a466ef4",
+    "url": "https://abufawwaz.github.io/csb-0k5ue/static/css/2.9fe0ca66.chunk.css"
+  },
+  {
+    "revision": "d13977d2df2aa0c71041",
+    "url": "https://abufawwaz.github.io/csb-0k5ue/static/css/main.44d0c857.chunk.css"
+  },
+  {
+    "revision": "95b3eec258064a466ef4",
+    "url": "https://abufawwaz.github.io/csb-0k5ue/static/js/2.1f81b93f.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "https://abufawwaz.github.io/csb-0k5ue/static/js/2.1f81b93f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d13977d2df2aa0c71041",
+    "url": "https://abufawwaz.github.io/csb-0k5ue/static/js/main.57288265.chunk.js"
+  },
+  {
+    "revision": "49172180f6c4be4cdb0b",
+    "url": "https://abufawwaz.github.io/csb-0k5ue/static/js/runtime-main.3b5bff17.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "https://abufawwaz.github.io/csb-0k5ue/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "https://abufawwaz.github.io/csb-0k5ue/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "https://abufawwaz.github.io/csb-0k5ue/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "https://abufawwaz.github.io/csb-0k5ue/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "https://abufawwaz.github.io/csb-0k5ue/static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
